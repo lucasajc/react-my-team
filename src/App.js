091d283aa27dd77aeb9485052a131886417298ad
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2018-09-16 19:31:17 -0300

    feat: menu, search container, user toggle, new member button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 import './style/tabs.css';
 import { Route } from "react-router-dom";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+import { faSortDown } from "@fortawesome/free-solid-svg-icons";
 
 class App extends Component {
   render() {
@@ -16,17 +20,54 @@ class App extends Component {
             <div role="group">
               <aside
                 className="menuNav">
-
+                <nav className="active-menu-item-container">
+                  <FontAwesomeIcon
+                            className="active-menu-icon"
+                            icon={faCircle}
+                          />
+                </nav>
+                <nav className="menu-item-container">
+                  <FontAwesomeIcon
+                            className="menu-icon"
+                            icon={faCircle}
+                          />
+                  <h3>TASKS</h3>
+                </nav>
+                <nav className="menu-item-container">
+                  <FontAwesomeIcon
+                            className="menu-icon"
+                            icon={faCircle}
+                          />
+                  <h3>CONTENTS</h3>
+                </nav>
+                <nav className="menu-item-container">
+                  <FontAwesomeIcon
+                            className="menu-icon"
+                            icon={faCircle}
+                          />
+                  <h3>SETUP</h3>
+                </nav>
               </aside>
               <aside
                 className="tabNav">
               </aside>
               <header>
                 <h1 className="title-like">Setup</h1>
+                <nav className="menu-drop-down">
+                  <h2 className="drop-down-like">Olá, </h2>
+                  <FontAwesomeIcon
+                          className="drop-down-icon"
+                          icon={faSortDown}
+                        />
+                </nav>
+                
               </header>
-              <nav>
+              <nav className="panelContainer">
+              
+                    <div className="tabPanelContainer"></div>
                 <Tabs>
                   <TabList>
+
                     <Tab>MINHA EQUIPE</Tab>
                     <Tab>TAB ITEM</Tab>
                     <Tab>TAB ITEM</Tab>
@@ -35,6 +76,32 @@ class App extends Component {
                   </TabList>
 
                   <TabPanel>
+
+                    <nav className="list-names-wrapper">
+                      <nav className="search-names-input-wrapper">
+                        <FontAwesomeIcon
+                          className="search-icon"
+                          icon={faSearch}
+                        />
+                        <input
+                          type="text"
+                          placeholder="Digite uma palavra-chave e tecle enter para buscar"
+                          className="search-names-input"
+                        />
+                      </nav>
+
+                      <nav className="manage-users-wrapper">
+                        <p>Visualizar:
+                          <button type="button" className="button-user-off">FREELAS</button>
+                          <button type="button" className="button-user-on">USUÁRIOS</button>
+                          <button type="button" className="button-user-add">NOVO MEMBRO</button>
+                        </p>
+                      </nav>
+
+                    </nav>
+
+
+                     
                   </TabPanel>
                   <TabPanel>
                   </TabPanel>
